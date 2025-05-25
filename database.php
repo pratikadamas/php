67d49c07c3fbe2657e4b2141b556b1fc298ce692
pratikadamas <147491382+pratikadamas@users.noreply.github.com>
2025-05-26 00:28:39 +0530

    database connection in php

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "miniproject";
+$db_port = "3307";
+$conn="";
+try {
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name, $db_port);
+} catch (Exception $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+if (!$conn) {
+    // die("Connection failed: " . mysqli_connect_error());
+    echo "Error: ";
+} else if ($conn) {
+    // echo "Connected successfully";
+}
+?>
